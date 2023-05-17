f5bb76606e4c50989ac85d1079808d66d03299af
Mohammed Mahani <mohammed.mahanu@gmail.com>
2023-05-17 12:54:59 +0300

    create search input function Relates#7 closes #7

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,4 +1,4 @@
-const getData = () => {
+export const getData = () => {
   return fetch(`http://ddragon.leagueoflegends.com/cdn/12.5.1/data/en_US/champion.json`)
     .then(res => res.json())
     .catch((err) => {
@@ -6,4 +6,12 @@ const getData = () => {
     });
 };
 
-export default getData;
+
+export const getChampData = (name) => {
+  return fetch(`http://ddragon.leagueoflegends.com/cdn/13.9.1/data/en_US/champion/${name}.json
+  `)
+    .then(res => res.json())
+    .catch((err) => {
+      throw new Error(`fetch getData failed ${err}`);
+    });
+};
